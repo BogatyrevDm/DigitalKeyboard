ee4f2d2e5583796a5ee79423201a575e4cf30bf2
Bogatyrev Dmitrii <bogatyrevdm82@gmail.com>
2021-01-20 21:26:21 +0300

    adding sharedPreferences

--- app/src/main/java/com/example/digitalkeyboard/BaseActivity.java
+++ app/src/main/java/com/example/digitalkeyboard/BaseActivity.java
@@ -1,13 +1,49 @@
 package com.example.digitalkeyboard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
+    protected final static int MAIN_THEME = 0;
+    protected final static int PSYCHEDELIC_THEME = 1;
+    protected final static int RED_THEME = 2;
+    private static final String APP_THEME = "APP_THEME";
+    private static final String PREFERENCES_NAME = "CALCULATOR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(getAppTheme(R.style.Theme_DigitalKeyboard));
+    }
+
+    private int getAppTheme(int themeId) {
+        return codeStyleToStyleId(getCodeStyle(themeId));
+    }
+
+    protected int getCodeStyle(int appThemeDefault) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+        return sharedPreferences.getInt(APP_THEME, appThemeDefault);
+    }
+
+    protected void setAppTheme(int codeStyle) {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(APP_THEME, codeStyle);
+        editor.apply();
+    }
+
+    private int codeStyleToStyleId(int codeStyle) {
+        switch (codeStyle) {
+            case MAIN_THEME:
+                return R.style.Theme_DigitalKeyboard;
+            case PSYCHEDELIC_THEME:
+                return R.style.Theme_Psychedelic;
+            case RED_THEME:
+                return R.style.Theme_Red;
+            default:
+                return R.style.Theme_DigitalKeyboard;
+        }
     }
 }
--- app/src/main/java/com/example/digitalkeyboard/SettingsActivity.java
+++ app/src/main/java/com/example/digitalkeyboard/SettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.digitalkeyboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivity extends BaseActivity {
 
@@ -10,5 +12,34 @@ public class SettingsActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        initThemeChooser();
+        initButtons();
+    }
+
+    private void initButtons() {
+        Button buttonOk = findViewById(R.id.button_ok);
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        }
+
+
+        );
+    }
+
+    private void initThemeChooser() {
+        initRadioButtonListener(findViewById(R.id.radio_button_main_theme), MAIN_THEME);
+        initRadioButtonListener(findViewById(R.id.radio_button_psychedelic_theme), PSYCHEDELIC_THEME);
+        initRadioButtonListener(findViewById(R.id.radio_button_red_theme), RED_THEME);
+    }
+
+    private void initRadioButtonListener(View viewById, int mainTheme) {
+
+
+        viewById.setOnClickListener(v -> {
+
+        });
     }
 }
